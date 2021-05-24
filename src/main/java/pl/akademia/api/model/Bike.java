330d5f9bc3b2a83167ffda0b30eb86cb3dcf6821
operchalskab <operchalska@wp.pl>
2021-05-24 19:52:30 +0200

    add JPA to BikeRepository

---
+++ src/main/java/pl/akademia/api/model/Bike.java
@@ -0,0 +1,31 @@
+package pl.akademia.api.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "bike")
+public class Bike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "type", nullable = false, length = 15)
+    private String type;
+
+    @Column(name = "brand", nullable = false, length = 20)
+    private String brand;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "stock", nullable = false, )
+    private int stock;
+
+
+
+}
