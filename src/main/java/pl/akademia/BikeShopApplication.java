3aa400feb663593c17cb07e1f25bc5c88f4dd819
Lukasz Dusza <lukaszdusza280@gmail.com>
2021-05-24 18:22:14 +0200

    init

---
+++ src/main/java/pl/akademia/BikeShopApplication.java
@@ -0,0 +1,13 @@
+package pl.akademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeShopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BikeShopApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/akademia/BikeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akademia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeShopApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
