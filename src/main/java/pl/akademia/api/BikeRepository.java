14c94c6d88fead8e1647d396d02e9276965a41be
Mariusz <mariusz_podsiadlly@o2.pl>
2021-05-24 19:52:10 +0200

    add @Repository to BikeRepository

--- src/main/java/pl/akademia/api/BikeRepository.java
+++ src/main/java/pl/akademia/api/BikeRepository.java
@@ -1,8 +1,10 @@
 package pl.akademia.api;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.akademia.api.model.Bike;
 
+@Repository
 public interface BikeRepository extends JpaRepository<Bike,Long> {
 
 }
