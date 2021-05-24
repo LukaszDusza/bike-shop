dcdf6c36bcbd68bfe1b36068a26cbc01db2587ab
Lukasz Dusza <lukaszdusza280@gmail.com>
2021-05-24 19:47:47 +0200

    add Bike entity

--- src/main/java/pl/akademia/api/BikeRepository.java
+++ src/main/java/pl/akademia/api/BikeRepository.java
@@ -1,10 +1,4 @@
 package pl.akademia.api;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.akademia.api.model.Bike;
-
-@Repository
-public interface BikeRepository extends JpaRepository<Bike, Long> {
-
+public interface BikeRepository {
 }
