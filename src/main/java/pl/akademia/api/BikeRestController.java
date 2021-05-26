12107faf32352fa4022d745b1fc9687a058f8e2b
Mariusz <mariusz_podsiadlly@o2.pl>
2021-05-26 20:03:56 +0200

    add @GetMapping x2 and @PostMapping

--- src/main/java/pl/akademia/api/BikeRestController.java
+++ src/main/java/pl/akademia/api/BikeRestController.java
@@ -1,4 +1,49 @@
 package pl.akademia.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.akademia.api.model.Bike;
+
+import java.util.List;
+
+//@Controller // szukal widokow, zwracl pliki stringowe do html
+@RestController
+@RequestMapping("/api/v1")
 public class BikeRestController {
+
+    private final BikeService bikeService;
+
+
+    public BikeRestController(BikeService bikeService) {
+        this.bikeService = bikeService;
+    }
+
+    //opakowuje odpowiedz z seriwsu , encja odpowiedzi
+
+    @GetMapping("/bikes")
+    public ResponseEntity<List<Bike>> getAllBikes() {
+        List<Bike> bikes = bikeService.getAllBikes();
+        if(bikes.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(bikes, HttpStatus.OK);
+    }
+
+    @GetMapping("bikes/{id}")
+    public ResponseEntity<Bike> getBike(@PathVariable long id){
+        Bike bike = bikeService.getBikeById(id);
+        if(bike==null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(bike,HttpStatus.FOUND);
+    }
+
+    @PostMapping("/bikes")
+    public ResponseEntity<Bike> createBike(@RequestBody Bike bike){
+            bikeService.createBike(bike);
+        return  new ResponseEntity<>(bike,HttpStatus.CREATED);
+    }
+
+
 }
