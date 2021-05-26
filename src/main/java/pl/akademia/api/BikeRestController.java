9be96bd216dd676d8f2d25f2ea13e89e907fc000
Mateusz <mateusz.luzargm@gmail.com>
2021-05-26 18:47:10 +0200

    add endpoint /bikes

--- src/main/java/pl/akademia/api/BikeRestController.java
+++ src/main/java/pl/akademia/api/BikeRestController.java
@@ -1,4 +1,32 @@
 package pl.akademia.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.akademia.api.model.Bike;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class BikeRestController {
+
+    private BikeService bikeService;
+
+    public BikeRestController(BikeService bikeService) {
+        this.bikeService = bikeService;
+    }
+
+    @GetMapping("/bikes")
+    public ResponseEntity<List<Bike>> getAllBikes() {
+        List<Bike> bikes = bikeService.getAllBikes();
+        if(bikes.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(bikes, HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/pl/akademia/api/BikeService.java
+++ src/main/java/pl/akademia/api/BikeService.java
@@ -1,4 +1,20 @@
 package pl.akademia.api;
 
+import org.springframework.stereotype.Service;
+import pl.akademia.api.model.Bike;
+
+import java.util.List;
+
+@Service
 public class BikeService {
+
+    private final BikeRepository bikeRepository;
+
+    public BikeService(BikeRepository bikeRepository) {
+        this.bikeRepository = bikeRepository;
+    }
+
+    public List<Bike> getAllBikes() {
+        return bikeRepository.findAll();
+    }
 }
