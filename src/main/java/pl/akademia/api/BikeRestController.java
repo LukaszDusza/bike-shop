070728feaadf1127b1900bc3120228ed90e913b8
Lukasz Dusza <lukaszdusza280@gmail.com>
2021-05-26 18:47:54 +0200

    add endpoint /bikes

--- src/main/java/pl/akademia/api/BikeRestController.java
+++ src/main/java/pl/akademia/api/BikeRestController.java
@@ -1,7 +1,13 @@
 package pl.akademia.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.akademia.api.model.Bike;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -11,6 +17,15 @@ public class BikeRestController {
 
   public BikeRestController(BikeService bikeService) {
     this.bikeService = bikeService;
+  }
+
+  @GetMapping("/bikes")
+  public ResponseEntity<List<Bike>> getAllBikes() {
+    List<Bike> bikes = bikeService.getAllBikes();
+    if(bikes.isEmpty()) {
+      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    return new ResponseEntity<>(bikes, HttpStatus.OK);
   }
 
 
