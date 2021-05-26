deabd7ccc17b61fb918913d13636dcde64ecd0fc
Mariusz <mariusz_podsiadlly@o2.pl>
2021-05-26 20:02:54 +0200

    swaggerConfig

---
+++ src/main/java/pl/akademia/api/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package pl.akademia.api;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean // odpala spring zwsze automatycznie
+    public Docket swagger(){
+        return new Docket (DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("pl.akademia.api"))
+                .paths(regex("/api.*"))
+                .build();
+
+    }
+
+
+}
