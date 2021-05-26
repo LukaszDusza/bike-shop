5185d13b760ea38ffe037db8011e6fa224722119
Lukasz Dusza <lukaszdusza280@gmail.com>
2021-05-26 18:22:01 +0200

    add dependencies to component classes

--- src/main/java/pl/akademia/api/BikeRestController.java
+++ src/main/java/pl/akademia/api/BikeRestController.java
@@ -1,4 +1,17 @@
 package pl.akademia.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class BikeRestController {
+
+  private final BikeService bikeService;
+
+  public BikeRestController(BikeService bikeService) {
+    this.bikeService = bikeService;
+  }
+
+
 }
--- src/main/java/pl/akademia/api/BikeService.java
+++ src/main/java/pl/akademia/api/BikeService.java
@@ -1,4 +1,13 @@
 package pl.akademia.api;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BikeService {
+
+  private final BikeRepository bikeRepository;
+
+  public BikeService(BikeRepository bikeRepository) {
+    this.bikeRepository = bikeRepository;
+  }
 }
