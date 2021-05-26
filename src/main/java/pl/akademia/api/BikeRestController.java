d329f73dc047cfe797f3fba653f00e07800f9a1b
Kamil Paprocki <kamil.paprocki1@gmail.com>
2021-05-26 18:21:59 +0200

    add dependencies to comoponent classes

--- src/main/java/pl/akademia/api/BikeRestController.java
+++ src/main/java/pl/akademia/api/BikeRestController.java
@@ -1,4 +1,15 @@
 package pl.akademia.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
 public class BikeRestController {
+
+    private final BikeService bikeService;
+
+    public BikeRestController(BikeService bikeService) {
+        this.bikeService = bikeService;
+    }
 }
