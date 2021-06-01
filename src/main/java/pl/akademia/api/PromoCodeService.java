fbab462e59965f3ad35433643b226da4af19c919
Kamil Paprocki <kamil.paprocki1@gmail.com>
2021-06-01 19:11:34 +0200

    add createPromoCode()

--- src/main/java/pl/akademia/api/PromoCodeService.java
+++ src/main/java/pl/akademia/api/PromoCodeService.java
@@ -1,10 +1,48 @@
 package pl.akademia.api;
 
 import org.springframework.stereotype.Service;
+import pl.akademia.api.model.Order;
+import pl.akademia.api.model.PromoCode;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 @Service
 public class PromoCodeService {
+    private  final int maxMonthsExp = 6;
+    private  final int minMonthsExp = 1;
+    private  final BigDecimal maxDiscount = new BigDecimal(500);
+    private  final BigDecimal minDiscount = new BigDecimal(50);
+    private  final int maxMultipleUse = 20;
+
+
+    public final PromoCodeRepository promoCodeRepository;
+
+    public PromoCodeService(PromoCodeRepository promoCodeRepository) {
+        this.promoCodeRepository = promoCodeRepository;
+    }
+
+    public PromoCode createPromoCode(Order order){
+
+        PromoCode promoCode = new PromoCode();
+        promoCode.setPromoCode(UUID.randomUUID());
+        promoCode.setOrder_id(order.getId());
+        promoCode.setGenerateDate(LocalDateTime.now());
+        promoCode.setExpDate(promoCode.getGenerateDate().plusMonths(ThreadLocalRandom.current().nextInt(minMonthsExp, maxMonthsExp+1)));
+        promoCode.setDiscount(new BigDecimal(ThreadLocalRandom.current().nextInt(minDiscount.intValue(), maxDiscount.intValue())));
+        promoCode.setMultipleUse(ThreadLocalRandom.current().nextBoolean());
+        if (promoCode.isMultipleUse()){
+          promoCode.setMultipleUse(ThreadLocalRandom.current().nextInt(2, maxMultipleUse));
+        } else
+            promoCode.setMultipleUse(1);
+        return promoCodeRepository.save(promoCode);
+    }
 
 
 }
--- src/main/java/pl/akademia/api/model/PromoCode.java
+++ src/main/java/pl/akademia/api/model/PromoCode.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
@@ -18,28 +18,28 @@ public class PromoCode {
     @Column(name = "promo_code_id")
     Long id;
 
-    @Column(name = "promo_code")
+    @Column(name = "promo_code", nullable = false)
     UUID promoCode;
 
-    @Column(name = "order_id")
+    @Column(name = "order_id", nullable = false)
     Long order_id;
 
     @Column(name = "used_date")
-    Date usedDate;
+    LocalDateTime usedDate;
 
-    @Column(name = "generate_date")
-    Date generateDate;
+    @Column(name = "generate_date", nullable = false)
+    LocalDateTime generateDate;
 
-    @Column(name = "exp_date")
-    Date expDate;
+    @Column(name = "exp_date", nullable = false)
+    LocalDateTime expDate;
 
-    @Column(name = "discount")
+    @Column(name = "discount", nullable = false)
     BigDecimal discount;
 
-    @Column(name = "is_multiple_use")
+    @Column(name = "is_multiple_use", nullable = false)
     boolean isMultipleUse;
 
-    @Column(name = "multiple_use")
+    @Column(name = "multiple_use", nullable = false)
     int multipleUse;
 
 
