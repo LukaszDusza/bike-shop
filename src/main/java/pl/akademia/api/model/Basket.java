60848c75e4aaee793ea6d18d10ddd2c09df236d4
Mariusz <mariusz_podsiadlly@o2.pl>
2021-06-01 17:56:33 +0200

    add zad3,4 Order,Basket

---
+++ src/main/java/pl/akademia/api/model/Basket.java
@@ -0,0 +1,28 @@
+package pl.akademia.api.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table (name = "basket")
+public class Basket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "total_price")
+    private BigDecimal basketTotalPrice;
+
+
+    @OneToMany // (mappedBy = "basket")
+    private List<Bike> bikes = new ArrayList<>();
+
+
+}
