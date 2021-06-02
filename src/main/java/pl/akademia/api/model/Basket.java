c517b527a62dcebe4616b7b44a686dcdf8eef35c
Mariusz <mariusz_podsiadlly@o2.pl>
2021-06-02 19:17:04 +0200

    changes : Date sql, Blik paymentMethod,

--- src/main/java/pl/akademia/api/model/Basket.java
+++ src/main/java/pl/akademia/api/model/Basket.java
@@ -1,7 +1,6 @@
 package pl.akademia.api.model;
 
 import lombok.Data;
-
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.ArrayList;
--- src/main/java/pl/akademia/api/model/Order.java
+++ src/main/java/pl/akademia/api/model/Order.java
@@ -1,7 +1,6 @@
 package pl.akademia.api.model;
 
 import lombok.Data;
-
 import javax.persistence.*;
 import java.sql.Date;
 
