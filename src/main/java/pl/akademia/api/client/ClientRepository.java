d776e9d1ce37c7a825aca23090c5330ba49981e9
Lukasz Dusza <lukaszdusza280@gmail.com>
2021-06-15 19:29:40 +0200

    BUGFIX ClientRepository

--- src/main/java/pl/akademia/api/client/ClientRepository.java
+++ src/main/java/pl/akademia/api/client/ClientRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ClientRepository extends JpaRepository<Client,Long> {
 
-  @Query(value = "select  from Client c where c.email = :email")
+  @Query(value = "select c from Client c where c.email = :email")
   Client getClientByEmail(String email);
 
 }
