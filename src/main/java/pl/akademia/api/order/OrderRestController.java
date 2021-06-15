8a7ba1c1ea4e20d25953156f46c8a8a5b24c101e
aklekot <74aklekot@gmail.com>
2021-06-15 17:50:13 +0200

    Order By Client or By Brand

--- src/main/java/pl/akademia/api/order/OrderRestController.java
+++ src/main/java/pl/akademia/api/order/OrderRestController.java
@@ -55,4 +55,22 @@ public class OrderRestController {
         return new ResponseEntity<>(orders,HttpStatus.OK);
     }
 
+    @GetMapping("/orders/client/{id}")
+    public ResponseEntity<List<Order>> getOrderByClientId(@PathVariable Long id){
+        List<Order> orders= orderService.getOrderByClientId(id);
+        if (orders.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(orders,HttpStatus.OK);
+    }
+
+    @GetMapping("/orders/bikes/{brand}")
+    public ResponseEntity<List<Order>> getOrderByBikeBrand(@PathVariable String brand){
+        List<Order> orders= orderService.getOrderByBikeBrand(brand);
+        if (orders.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(orders,HttpStatus.OK);
+    }
+
 }
--- src/main/java/pl/akademia/api/order/OrderService.java
+++ src/main/java/pl/akademia/api/order/OrderService.java
@@ -23,21 +23,16 @@ public class OrderService {
         return orderRepository.findById(id).orElse(null);
     }
 
-    public Order getOrderById(Long id){return orderRepository.findById(id).orElse(null);}
-
     public List<Order> getOrderByDate(Date date){return orderRepository.getOrderByDate(date);}
 
-//    public Order createOrder(Order order) {
-//        return orderRepository.save(order);
-//    }
+    public List<Order> getOrderBySize(Integer min, Integer max){return orderRepository.getOrderBySize(min, max);}
+
 
     public List<Order> getOrderByClientId(Long id) {
-
         return orderRepository.getOrderByClientId(id);
         }
 
         public List<Order> getOrderByBikeBrand(String brand) {
-
         return orderRepository.getOrderByBikeBrand(brand);
         }
 }
