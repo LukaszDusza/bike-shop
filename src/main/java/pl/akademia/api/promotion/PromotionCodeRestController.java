cd6830db0261de52a3c73c730476e9a53cc18743
Kamil Paprocki <kamil.paprocki1@gmail.com>
2021-06-22 17:10:02 +0200

    add logger into PromotionCodeRestController

--- src/main/java/pl/akademia/api/promotion/PromotionCodeRestController.java
+++ src/main/java/pl/akademia/api/promotion/PromotionCodeRestController.java
@@ -1,5 +1,7 @@
 package pl.akademia.api.promotion;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import java.util.List;
 public class PromotionCodeRestController {
 
     private final PromotionCodeService promotionCodeService;
+    Logger logger = LoggerFactory.getLogger(PromotionCodeRestController.class);
 
     public PromotionCodeRestController(PromotionCodeService promotionCodeService) {
         this.promotionCodeService = promotionCodeService;
@@ -42,20 +45,25 @@ public class PromotionCodeRestController {
             if (promotionCodes.isEmpty()) return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             return new ResponseEntity<>(promotionCodes, HttpStatus.OK);
         }
+        logger.error("Wrong request parameter: {}", promoCodeStatus);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("/promocodes/{promocode}")
-    public ResponseEntity<PromotionCode> getPromotionCodeByCode(@RequestParam String promocode){
-        if (promotionCodeService.getPromotionCodeByCode(promocode) == null) return new ResponseEntity<>(promotionCodeService.getPromotionCodeByCode(promocode), HttpStatus.NOT_FOUND);
-        return new ResponseEntity<>(promotionCodeService.getPromotionCodeByCode(promocode), HttpStatus.OK);
+    public ResponseEntity<PromotionCode> getPromotionCodeByCode(@RequestParam String promotionCode){
+        if (promotionCodeService.getPromotionCodeByCode(promotionCode) == null) {
+            logger.error("Wrong request parameter: {}", promotionCode);
+            return new ResponseEntity<>(promotionCodeService.getPromotionCodeByCode(promotionCode), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(promotionCodeService.getPromotionCodeByCode(promotionCode), HttpStatus.OK);
     }
 
-    @PostMapping("/promocodes/{promocode}/use")
-    public ResponseEntity<PromotionCode> usePromotionCode(@PathVariable String promocode){
+    @PostMapping("/promocodes/{promotionCode}/use")
+    public ResponseEntity<PromotionCode> usePromotionCode(@PathVariable String promotionCode){
         try{
-            promotionCodeService.usePromotionCode(promocode);
+            promotionCodeService.usePromotionCode(promotionCode);
         } catch(WrongPromotionCodeException e){
+            logger.error("Wrong request parameter: {}", promotionCode);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(HttpStatus.OK);
@@ -63,13 +71,19 @@ public class PromotionCodeRestController {
 
     @GetMapping("/promocode/{id}")
     public ResponseEntity<PromotionCode> getPromotionCodeById(@PathVariable Long id){
-        if (promotionCodeService.getPromotionCodeById(id) == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (promotionCodeService.getPromotionCodeById(id) == null) {
+            logger.error("Wrong ID in request parameter: {}", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
         return new ResponseEntity<>(promotionCodeService.getPromotionCodeById(id), HttpStatus.OK);
     }
     @DeleteMapping("/promocode/{id}/delete")
     public ResponseEntity<PromotionCode> deletePromotionCodeById(@PathVariable Long id){
         if (promotionCodeService.deletePromotionCodesById(id) > 0) return new ResponseEntity<>(HttpStatus.OK);
-        else return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+        else {
+            logger.error("Wrong ID in request parameter: {}", id);
+            return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+        }
     }
 
 
