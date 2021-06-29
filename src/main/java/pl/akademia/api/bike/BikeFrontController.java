b523428cb8e6853bf9b55b21bb45148407e59fb3
lucky-luk <lukasz.lesko@tlen.pl>
2021-06-29 21:36:00 +0200

    add bikeAdd form

--- src/main/java/pl/akademia/api/bike/BikeFrontController.java
+++ src/main/java/pl/akademia/api/bike/BikeFrontController.java
@@ -3,18 +3,30 @@ package pl.akademia.api.bike;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BikeFrontController {
 
   private BikeService bikeService;
 
-  @GetMapping("/")
-  public String getHomePage(Model model) {
-    //todo uzupelnic rowery
-    return "index";
+  public BikeFrontController(BikeService bikeService) {
+    this.bikeService = bikeService;
   }
 
+  @GetMapping("/bike")
+  public String getBikePage(Model model, @RequestParam(required = false) String info) {
+    if (info != null) {
+      model.addAttribute("info", info);
+    }
+    return "bike";
+  }
 
-
+  @PostMapping("/add/bike")
+  public String addBike(@ModelAttribute Bike bike) {
+    bikeService.createOrUpdateBike(bike);
+    return "redirect:/bike?info=Rower zostal zaktualizowany/dodany";
+  }
 }
