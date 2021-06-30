e68a763b8146d8b8c21b255af557790a94436c6a
Lukasz Dusza <lukaszdusza280@gmail.com>
2021-06-30 19:07:51 +0200

    add some stuff

--- src/main/java/pl/akademia/api/client/ClientFrontController.java
+++ src/main/java/pl/akademia/api/client/ClientFrontController.java
@@ -1,21 +1,33 @@
 package pl.akademia.api.client;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 
 @Controller
 public class ClientFrontController {
 
-    private ClientService clientService;
+    private final ClientService clientService;
 
     public ClientFrontController(ClientService clientService){
         this.clientService=clientService;
     }
 
+    @GetMapping("/client")
+    public String clientPage(){
+        return "form-client";
+    }
+
     @PostMapping("/add/client")
     public String addClient(@ModelAttribute Client client){
+        System.out.println(client);
+        client.setRegistrationDate(new java.sql.Date(Timestamp.valueOf(LocalDateTime.now()).getTime()));
         clientService.createOrUpdateClient(client);
-        return "form-client";
+        return "redirect:/client";
     }
 }
