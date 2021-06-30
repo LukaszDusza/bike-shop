66d2ea741329870b67a3ec417ce3566e28e859df
aklekot <74aklekot@gmail.com>
2021-06-30 15:59:25 +0200

    Frontend for File

---
+++ src/main/java/pl/akademia/api/file/FileFrontController.java
@@ -0,0 +1,38 @@
+package pl.akademia.api.file;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+
+@Controller
+public class FileFrontController {
+    private FileService fileService;
+
+    public FileFrontController(FileService fileService) {
+
+        this.fileService = fileService;
+    }
+
+@GetMapping("/files")
+public String getHomePage(Model model) {
+    return "index";
+}
+
+@PostMapping("/files")
+public String uploadFile(@RequestParam("file") MultipartFile file,
+                         RedirectAttributes redirectAttributes) throws IOException {
+       if (file.isEmpty()){
+           redirectAttributes.addFlashAttribute("message","Select a file to upload");
+       }
+        fileService.uploadFile(file);
+    redirectAttributes.addFlashAttribute("message","File is successfully uploaded"+file.getOriginalFilename());
+    return "upload";
+}
+}
