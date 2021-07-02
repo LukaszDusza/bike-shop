a3577d4501e09eb1a3331e2852f851fd2b07dba9
lucky-luk <lukasz.lesko@tlen.pl>
2021-07-02 11:25:05 +0200

    add bike xls file creator: in FileService add createBikeXlsFile() in BikeFrontController and BikeRestController add getBikeXlsFile() in bike.html add button

--- src/main/java/pl/akademia/api/bike/BikeFrontController.java
+++ src/main/java/pl/akademia/api/bike/BikeFrontController.java
@@ -1,21 +1,31 @@
 package pl.akademia.api.bike;
 
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.akademia.api.file.FileService;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 @Controller
 public class BikeFrontController {
 
-  private BikeService bikeService;
+  private final BikeService bikeService;
+  private final FileService fileService;
 
-  public BikeFrontController(BikeService bikeService) {
+  public BikeFrontController(BikeService bikeService, FileService fileService) {
     this.bikeService = bikeService;
+    this.fileService = fileService;
   }
 
   @GetMapping("/bike")
@@ -71,5 +81,21 @@ public class BikeFrontController {
   @GetMapping("/getBikeBySerialNumber")
   public String getBikeBySerialNumber(@RequestParam String serialNumber) {
     return "redirect:/bike?getBikeBySerialNumber="+serialNumber;
+  }
+
+  @GetMapping("/getBikeXlsFile")
+  public void getBikeXlsFile(HttpServletResponse response) throws IOException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+    String fileName = fileService.createBikeXlsFile();
+    Resource resource = fileService.getFile(fileName);
+    String fileUri = Paths.get(resource.getURI()).toString();
+    response.setContentType("application/vnd.ms-excel");
+    String headerKey = "Content-Disposition";
+    String headerValue = String.format("attachment; filename=\"%s\"", fileName);
+    response.setHeader(headerKey, headerValue);
+    FileInputStream inputStream = new FileInputStream(fileUri);
+    int c;
+    while ((c = inputStream.read()) != -1) {
+      response.getWriter().write(c);
+    }
   }
 }
